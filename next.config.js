49dde7c63f97b5be90376120b4bbd8bd4dbf239b
Trần Kim Thắng <111619268+KeyT9999@users.noreply.github.com>
2026-03-20 17:15:32 +0700

    Fix Vercel build trace stack overflow for public folder

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'public/**/*',
+        '.git/**/*',
+        'node_modules/**/*',
+      ],
+    },
+  },
 }
 
 module.exports = nextConfig
